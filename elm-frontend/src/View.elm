6f7c24b18b0abed7d1a2c31cb69ae8017b4ab992
Andre Wolden <wolden.andre@gmail.com>
2021-01-21 10:45:18 +0100

    adding background image and some other content

--- elm-frontend/src/View.elm
+++ elm-frontend/src/View.elm
@@ -1,7 +1,8 @@
 module View exposing (..)
 
 import Debug exposing (todo)
-import Element exposing (Element, centerX, column, el, explain, fill, maximum, paddingXY, paragraph, text, width)
+import Element exposing (Element, centerX, column, el, explain, fill, image, maximum, paddingXY, paragraph, spacing, text, textColumn, width)
+import Element.Background as Background exposing (uncropped)
 import Element.Font as Font
 import Html exposing (Html, div)
 import Model exposing (Model)
@@ -21,10 +22,12 @@ textFont =
 
 view : Model -> Html Msg
 view model =
-    Element.layout ([] ++ textFont)
+    Element.layout ([ Background.image "/images/stone-background.jpg" ] ++ textFont)
         (column ([ width <| maximum 1000 fill, centerX, explain todo ] ++ textFont)
             [ headerText
+            , el [ centerX, paddingXY 36 36 ] (image [] { src = "/images/Magnus-Main-Image-Vintage-Roll.gif", description = "aksjdnaksjnd" })
             , infoText
+            , paintings
             ]
         )
 
@@ -36,8 +39,20 @@ headerText =
 
 infoText : Element Msg
 infoText =
-    paragraph [ paddingXY 36 36 ]
-        [ text "Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. "
-        , text "Lorem ipsum og andre buskevekster"
-        , text "Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. "
+    textColumn [ paddingXY 56 36, spacing 24, width fill, centerX ]
+        [ paragraph [] [ text "Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. " ]
+        , paragraph [] [ text "Lorem ipsum og andre buskevekster" ]
+        , paragraph [] [ text "Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. Lorem ipsum og andre buskevekster. " ]
+        ]
+
+
+resString =
+    String.fromChar (Char.fromCode 0x25CF)
+
+
+paintings : Element Msg
+paintings =
+    column [ Font.size 48, paddingXY 36 36, spacing 36 ]
+        [ text (resString ++ "2015" ++ resString)
+        , image [] { src = "/images/artwork/2015/2015 A Loaded Message LARGE.jpg", description = "aksjdnaksjnd" }
         ]
